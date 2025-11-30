72329e723180f90873ec3d01e53f1fadf71019bc
Ali jawwad <33836051+jawwad-ali@users.noreply.github.com>
2025-11-30 22:53:59 +0500

    update

--- api/index.py
+++ api/index.py
@@ -4,4 +4,8 @@ app = FastAPI()
 
 @app.get("/api/health")
 async def health_check():
-    return {"status": "Test Driven DevelopmentTTTTTTTTTTTTTTTTTTTTT"}
+    return {"status": "Test Driven"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
